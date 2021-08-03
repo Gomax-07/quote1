685b3c58a460e5889a0f310b1ce54c0c069a728f
go-max07 <johnmetry17@gmail.com>
2021-08-03 08:31:41 +0300

    Made countClick method for upvote tally

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,6 +1,8 @@
+import { Quote } from './../models/Quote';
 import { Component, OnInit } from '@angular/core';
-import {QUOTES} from '../models/mock-quotes'
-import {Quote} from '../models/Quote'
+import { QUOTES } from '../models/mock-quotes';
+
+
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
@@ -9,15 +11,22 @@ import {Quote} from '../models/Quote'
 export class QuotesComponent implements OnInit {
   quotes = QUOTES;
   selectedQuote?: Quote;
-
+clickCounter: number = 0;
+clickCounter1: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
-
   }
 
-  OnSelect(quote: Quote):void {
+  onSelect(quote: Quote): void {
     this.selectedQuote = quote;
   }
+countClick(){
+  this.clickCounter += 1;
+}
+countClick1(){
+  this.clickCounter1 += 1;
+}
+
 }
